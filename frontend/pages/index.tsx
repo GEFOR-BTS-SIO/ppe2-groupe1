84dbfd5046f1d19f6a7f181652baaa2fe2d46e28
benedyczak <107350740+benedyczak@users.noreply.github.com>
2023-05-25 14:58:15 +0200

    redirection accueil(index) vers login

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,77 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import  BlueRoundButton  from '@/components/Bouton'
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
+const Login = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [token, setToken] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const router = useRouter();
 
-const inter = Inter({ subsets: ['latin'] })
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/login_check`,
+        {
+          username: data.username,
+          password: data.password,
+        }
+      );
+      localStorage.setItem("token", response.data.token);
+    
+      // Si la connexion réussit, stockez l'état de connexion de l'utilisateur et redirigez-le vers la page de profil
+      setIsAuthenticated(true);
+      setToken(response.data.token);
 
-export default function Home() {
+      router.push("/pagemessage");
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  const handleLogout = () => {
+    // Réinitialiser l'état de connexion de l'utilisateur et rediriger l'utilisateur vers la page de connexion
+    setIsAuthenticated(false);
+    router.push("/login");
+  };
+
   return (
-    <>
-<main>
-     </main>
-    </>
-  )
-}
+    <div>
+      {!isAuthenticated && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>Username:</label>
+            <input type="text" {...register("username", { required: true })} />
+            {errors.username && <span>Username is required</span>}
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              {...register("password", { required: true })}
+            />
+            {errors.password && <span>Password is required</span>}
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+          {errorMessage && <div>{errorMessage}</div>}
+        </form>
+      )}
+      {isAuthenticated && (
+        <div>
+          <p>You are logged in!</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
