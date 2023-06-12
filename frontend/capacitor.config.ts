9e7060100fe8541998aedc44231a41ec66c1721d
benedyczak <107350740+benedyczak@users.noreply.github.com>
2023-06-12 20:40:52 +0200

    fin apk  + modif login/index

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'projectreact',
-  webDir: 'public',
+  webDir: 'out',
   server: {
     androidScheme: 'https'
   }
